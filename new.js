1f0ddbf00dd329ec7af29fcff706a27a54624a5a
Baise129 <168041447+Baise129@users.noreply.github.com>
2024-12-02 12:41:06 +0000

    Add files via upload

---
+++ new.js
@@ -0,0 +1,10 @@
+
+let x = 5;
+let y = 6;
+let z = x + y;
+let age = 21;
+console.log(z);
+
+console.log(z);
+document.getElementById("p1").textContent = z;
+document.getElementById("p2").textContent =age ;
